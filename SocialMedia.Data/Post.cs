9a9becdb8c152e83a276947b162a3cda631ae51c
JustinnZiegler <justinnziegler@gmail.com>
2021-04-13 10:16:42 -0400

    Justinn's Branch

--- SocialMedia.Data/Post.cs
+++ SocialMedia.Data/Post.cs
@@ -8,5 +8,10 @@ namespace SocialMedia.Data
 {
     public class Post
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+        public Guid AuthorId { get; set; }
     }
 }
