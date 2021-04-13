9fa72897a6cc62e01b758e9f7c8bc2d8c88dd112
JustinnZiegler <justinnziegler@gmail.com>
2021-04-13 16:39:19 -0400

    Finished Extra Shit

---
+++ SocialMedia.Models/PostUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+   public class PostUpdate
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+    }
+}
