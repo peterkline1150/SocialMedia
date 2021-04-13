4e2db54b7b41944d9afef4b646f414697c5fc05a
JustinnZiegler <justinnziegler@gmail.com>
2021-04-13 11:29:20 -0400

    Finished Post

---
+++ SocialMedia.Services/PostService.cs
@@ -0,0 +1,59 @@
+﻿using SocialMedia.Data;
+using SocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Services
+{
+    public class PostService
+    {
+        private readonly Guid _userId;
+
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    AuthorId = _userId,
+                    Title = model.Title,
+                    Text = model.Text,
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<PostList> GetPosts()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Posts
+                        .Where(e => e.AuthorId == _userId)
+                        .Select(
+                            e =>
+                                new PostList
+                                {
+                                    Id = e.Id,
+                                    Title = e.Title,
+                                    Text = e.Text
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
